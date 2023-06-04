2e49669f21282cffe83c3767c7b27d82b8236722
DRMALEK <malekozel97@gmail.com>
2023-06-04 23:20:39 +0200

    config.py file is un-ignored

---
+++ config.py
@@ -0,0 +1,4 @@
+password = '1234'                               # e.g. password = '1234'
+database_name = 'database_v2_2.db'                               # e.g. database_name = 'database_1A'
+edit_mode_password = '1234'                               # e.g. edit_mode_password = 'allow_edit'
+dr_mls_access_code = '1234'      # e.g. dr_mls_access_code = 'access_auth'
